c370fdc1f01fb9e68e23c299b74faf068ed46491
Kate <kateiskra99@gmail.com>
2022-11-29 21:10:03 +0100

    add input event data binding

--- 04_data_binding/src/app/servers/servers.component.ts
+++ 04_data_binding/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -19,5 +20,9 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(): void {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: KeyboardEvent): void {
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 }
